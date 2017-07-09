38a2869f1cbb6a4d2e33fb0b986fe1ca1961929c
Chris Cummins <chrisc.101@gmail.com>
2017-07-09 10:47:16 +0100

    parse-log changes

--- parse-log.sh
+++ parse-log.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-set -eu
+set -u
 log=$1
 
-w=$(grep -c 'different output' $log)
-bf=$(grep -c 'failure (static)' $log)
-c=$(grep -E -c 'failure \((optimised|unoptimised|oracle)\)' $log)
-to=$(grep -c 'timeout' $log)
-tick=$(grep -c 'same output' $log)
-aborted=$(grep -c 'aborted' $log)
+w=$(grep -c 'different output' $log 2>/dev/null)
+bf=$(grep -c 'failure (static)' $log 2>/dev/null)
+c=$(grep -E -c 'failure \((optimised|unoptimised|oracle)\)' $log 2>/dev/null)
+to=$(grep -c 'timeout' $log 2>/dev/null)
+tick=$(grep -c 'same output' $log 2>/dev/null)
+aborted=$(grep -c 'aborted' $log 2>/dev/null)
 total=$((w+bf+c+to+tick+aborted))
 
+set -e
 (echo "w $w"
 echo "bf $bf"
 echo "c $c"
@@ -18,13 +19,13 @@ echo "✓ $tick"
 echo "aborted $aborted"
 echo "total $total") | column -t
 
-if [[ "$total" != 1000 ]]; then
-    echo
-    echo "wtf?"
-    grep -v '^+' $log | grep -v 'different output' \
-        | grep -v 'same output' \
-        | grep -E -v 'failure \((static|optimised|oracle)\)' \
-        | grep -v 'timeout' \
-        | grep -v 'same output' \
-        | grep -v 'aborted'
-fi
+# if [[ "$total" != 1000 ]]; then
+#     echo >&2
+#     echo "warning: incomplete" >&2
+#     # grep -v '^+' $log | grep -v 'different output' \
+#     #     | grep -v 'same output' \
+#     #     | grep -E -v 'failure \((static|optimised|oracle)\)' \
+#     #     | grep -v 'timeout' \
+#     #     | grep -v 'same output' \
+#     #     | grep -v 'aborted'
+# fi
